8adde09ffdff65256a002db9efedc2d0b0593039
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-03 15:36:39 -0700

    Convert ListAllUsers Struts Action.
     * Cleanup remaining struts references.

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -1,43 +1,12 @@
 package org.superbiz.struts;
 
-import com.opensymphony.sitemesh.webapp.SiteMeshFilter;
-import org.apache.struts2.dispatcher.filter.StrutsPrepareAndExecuteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import javax.servlet.Filter;
-
-import static java.util.Arrays.asList;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public FilterRegistrationBean sitemeshFilter() {
-        return buildFilterRegistration(0, new SiteMeshFilter());
-    }
-
-    @Bean
-    public FilterRegistrationBean strutsExecuteFilter() {
-        return buildFilterRegistration(1, new StrutsPrepareAndExecuteFilter());
-    }
-
-
-    private FilterRegistrationBean buildFilterRegistration(int order, Filter filter) {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(filter);
-        registration.setUrlPatterns(asList(
-            "/decorators/layout.jsp",
-            "/listAllUsers.action",
-            "/listAllUsers.jsp"
-        ));
-        registration.setOrder(order);
-        return registration;
     }
 }
